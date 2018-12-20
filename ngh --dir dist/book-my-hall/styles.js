bd3a2f6e8dbe10ccff7a9c93215b47d42661436c
ajith.j@invensis.net <ajith.j@invensis.net>
2018-12-20 13:34:57 +0530

    prepare for heroku

--- ngh --dir dist/book-my-hall/styles.js
+++ ngh --dir dist/book-my-hall/styles.js
@@ -639,9 +639,9 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Ajith J\book-my-hall\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
-__webpack_require__(/*! C:\Users\Ajith J\book-my-hall\src\styles.css */"./src/styles.css");
-module.exports = __webpack_require__(/*! C:\Users\Ajith J\book-my-hall\node_modules\font-awesome\css\font-awesome.css */"./node_modules/font-awesome/css/font-awesome.css");
+__webpack_require__(/*! /home/developer/ang/book-my-hall/node_modules/bootstrap/dist/css/bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
+__webpack_require__(/*! /home/developer/ang/book-my-hall/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/developer/ang/book-my-hall/node_modules/font-awesome/css/font-awesome.css */"./node_modules/font-awesome/css/font-awesome.css");
 
 
 /***/ })
