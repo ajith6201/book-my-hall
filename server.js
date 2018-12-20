2d68041f6e5eb96c4645938df8b6ab7081631e5e
ajith.j@invensis.net <ajith.j@invensis.net>
2018-12-20 13:43:20 +0530

    prepare for heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/book-my-hall'));
+app.use(express.static(__dirname + '/book-my-hall'));
 
 // Send all requests to index.html
 app.all('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/book-my-hall/index.html'));
+  res.sendFile(path.join(__dirname + '/book-my-hall/index.html'));
 });
 
 // default Heroku port
