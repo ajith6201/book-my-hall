664cfa39a76f28c9205726df90429045c195fefb
ajith <ajithjbdvt@gmail.com>
2019-01-05 12:42:22 +0530

    deploy to aws ec2

--- server.js
+++ server.js
@@ -4,21 +4,13 @@ var http=require('http');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist/book-my-hall'));
 
 // Send all requests to index.html
 app.all('*', function(req, res) {
-  res.status(200).sendFile(path.join(__dirname + '/index.html'));
-});
-var server=http.createServer(function(req,res){
-  res.end('test');
+  res.status(200).sendFile(path.join(__dirname + '/dist/book-my-hall/index.html'));
 });
 
-server.on('listening',function(){
-  console.log('ok, server is running');
-});
-
-server.listen(80);
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
 console.log(`Example app listening on port`);
