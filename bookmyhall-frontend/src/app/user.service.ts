7582359d3067437660203aee2f0aa1285cb02a85
ajith <ajithjbdvt@gmail.com>
2019-01-28 00:41:53 +0530

    changed url to ec2 instance ip address in user services

--- bookmyhall-frontend/src/app/user.service.ts
+++ bookmyhall-frontend/src/app/user.service.ts
@@ -6,25 +6,29 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
+  //url:String = 'http://localhost:3000/users';
+  //testing for aws ec2 instance
+  url:String = 'http://52.14.158.45:3000/users';
+
   constructor(private _http:HttpClient) { }
   
   register(body:any)
   {
-    return this._http.post('http://127.0.0.1:3000/users/register',body,{
+    return this._http.post(this.url+'/register',body,{
       observe:'body',
       headers:new HttpHeaders().append('Content-Type','application/json')
     });
   }
 
   login(body:any){
-    return this._http.post('http://localhost:3000/users/login', body,{
+    return this._http.post(this.url+'/login', body,{
       observe:'body'
     });
   }
 
   getUsername()
   {
-    return this._http.get('http://localhost:3000/users/username',{
+    return this._http.get(this.url+'/username',{
     observe: 'body',
     params: new HttpParams().append('token',localStorage.getItem('token'))
     });
